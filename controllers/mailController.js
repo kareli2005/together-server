0562fe081b0eccc4017d2c8d1c9bad247ca58332
kareli2005 <cotneqareli123@gmail.com>
2024-10-27 02:43:33 +0400

    update mail server

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -1,7 +1,12 @@
 const nodemailer = require('nodemailer')
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 const transporter = nodemailer.createTransport({
-  service: 'Gmail',
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
@@ -18,13 +23,13 @@ exports.sendRegistrationLink = async (req, res) => {
     to: email,
     subject: 'Registration Link',
     text: `Click this link to register: ${registrationLink}`,
-    html: '<p>Click this link to register: <a href={registrationLink}>Get Link</a></p>',
+    html: `<p>Click this link to register: <a href="${registrationLink}">Get Link</a></p>`,
   }
 
   try {
     await transporter.sendMail(mailOptions)
     res.status(200).json({message: 'Registration link sent successfully.'})
   } catch (error) {
-    res.status(500).json({error: 'Failed to send email.'})
+    res.status(500).json({error: error})
   }
 }
