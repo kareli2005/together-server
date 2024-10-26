f8e09d91403f47e2b2dab1efa8bd62515ff3b74d
kareli2005 <cotneqareli123@gmail.com>
2024-10-27 02:09:53 +0400

    changed url

--- routes/mailRoutes.js
+++ routes/mailRoutes.js
@@ -2,6 +2,6 @@ const express = require('express')
 const router = express.Router()
 const mailController = require('../controllers/mailController')
 
-router.post('/send-registration-link', mailController.sendRegistrationLink)
+router.post('/sendRegistrationLink', mailController.sendRegistrationLink)
 
 module.exports = router
