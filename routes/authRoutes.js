104324dd497fc144204c36c105577a6364156ae5
kareli2005 <cotneqareli123@gmail.com>
2024-10-27 03:46:12 +0400

    Initial commit

---
+++ routes/authRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+const authController = require('../controllers/authController')
+const { verifyToken } = require('../middleware/authMiddleware')
+
+
+router.post('/register', authController.registerUser)
+router.post('/login', authController.loginUser)
+router.post('/getUserProfile', verifyToken, authController.getUserProfile)
+router.get('/test', authController.test)
+
+
+module.exports = router
